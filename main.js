268cf597f7a3459d75967f70fabcdc8764d2ebde
Sebastian Tomala <sebastian.tomalas@gmail.com>
2021-01-28 22:22:40 +0100

    prevent from setting opposite direction

--- main.js
+++ main.js
@@ -1,32 +1,108 @@
 class Snake {
     constructor(head, board) {
-        this.head      = head;
-        this.board     = board;
-        this.headSize  = this.head.offsetWidth;
-        this.speed     = 200;
+        this.head             = head;
+        this.board            = board;
+        this.headSize         = this.head.offsetWidth;
+        this.speed            = 200;
+        this.prevDirection    = undefined;
+        this.currentDirection = undefined;
+        this.newGame();
+    }
+    newGame() {
+        this.head.style.left  = 0;
+        this.head.style.top   = 0;
+        this.prevDirection    = undefined;
+        this.currentDirection = undefined;
+        this.headX            = window.getComputedStyle(this.head).left;
+        this.headY            = window.getComputedStyle(this.head).top;
+    }
+    setPrevDirection(dir) {
         /* 
         'ArrowRight' - right; 
         'ArrowLeft' - left; 
         'ArrowUp' - up; 
         'ArrowDown' - down;
         */
-        this.newGame();
+        this.prevDirection = dir;
     }
-    newGame() {
-        this.head.style.left = 0;
-        this.head.style.top  = 0;
-        this.headX           = window.getComputedStyle(this.head).left;
-        this.headY           = window.getComputedStyle(this.head).top;
-        this.direction       = 'ArrowRight'; 
+    getPrevDirection() {
+        return this.prevDirection;
     }
-    setDirection(dir) {
-        this.direction = dir;
+    setCurrentDirection(dir) {
+        /* 
+        'ArrowRight' - right; 
+        'ArrowLeft' - left; 
+        'ArrowUp' - up; 
+        'ArrowDown' - down;
+        */
+        if(this.getPrevDirection() == undefined) {
+            this.setPrevDirection(dir);
+            this.currentDirection = dir;
+            return;
+        }
+        if(this.getPrevDirection() != undefined) {
+            
+            switch(dir) {
+                case 'ArrowRight':
+                    if(   (this.getPrevDirection() == 'ArrowRight')
+                        ||(this.getPrevDirection() == 'ArrowLeft')
+                        ) {
+                            return '';
+                        }else {
+                            this.currentDirection = dir;
+                            this.setPrevDirection(this.getCurrentDirection());
+                            return '';
+                        }
+                    break;
+                case 'ArrowLeft':
+                    if(   (this.getPrevDirection() == 'ArrowRight')
+                        ||(this.getPrevDirection() == 'ArrowLeft')
+                        ) {
+                            return '';
+                        }else {
+                            this.currentDirection = dir;
+                            this.setPrevDirection(this.getCurrentDirection());
+                            return '';
+                        }
+                    break;
+                case 'ArrowUp':
+                    if(   (this.getPrevDirection() == 'ArrowUp')
+                        ||(this.getPrevDirection() == 'ArrowDown')
+                        ) {
+                            return '';
+                        }else {
+                            this.currentDirection = dir;
+                            this.setPrevDirection(this.getCurrentDirection());
+                            return '';
+                        }
+                    break;
+                case 'ArrowDown':
+                    if(   (this.getPrevDirection() == 'ArrowUp')
+                        ||(this.getPrevDirection() == 'ArrowDown')
+                        ) {
+                            return '';
+                        }else {
+                            this.currentDirection = dir;
+                            this.setPrevDirection(this.getCurrentDirection());
+                            return '';
+                        }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
-    setSpeed() {
-
+    getCurrentDirection() {
+        return this.currentDirection;
+    }
+    setSpeed(value) {
+        return this.speed = value;
+    }
+    getSpeed() {
+        return this.speed;
     }
     move() {
-        switch(this.direction) {
+        switch(this.getCurrentDirection()) {
             case 'ArrowRight':
                 this.headX = parseInt(this.headX) + this.headSize + 'px';
                 break;
@@ -44,7 +120,12 @@ class Snake {
         }
         this.head.style.left = this.headX;
         this.head.style.top  = this.headY;
-        if((parseInt(this.headX) > 390) || (parseInt(this.headX) < 0) || (parseInt(this.headY) > 390) || (parseInt(this.headY) < 0)){
+        
+        if(    (parseInt(this.headX) > 390) 
+            || (parseInt(this.headX) < 0) 
+            || (parseInt(this.headY) > 390) 
+            || (parseInt(this.headY) < 0)
+            ) {
             this.newGame();
         }
     }
@@ -73,9 +154,9 @@ const snake = new Snake(head, board);
 document.body.addEventListener('keydown', 
 function(event) { 
     const key = event.key; 
-    if(directionArr.includes(key)) snake.setDirection(key);
+    if(directionArr.includes(key)) snake.setCurrentDirection(key);
 });
 
 setInterval(function() { 
     snake.move(); 
-}, snake.speed);
+}, snake.getSpeed());
